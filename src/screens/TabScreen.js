e9a30df579585c0257450aca54e8fa8414f26fbc
dhiyo7 <dhiyoreksa12@gmail.com>
2020-03-01 00:35:17 +0700

    First

---
+++ src/screens/TabScreen.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  Tab,
+  Tabs,
+  Left,
+  Right,
+  Body,
+  Title,
+} from 'native-base';
+import Tab1 from './tabs/tabOne';
+import Tab2 from './tabs/tabTwo';
+import Tab3 from './tabs/tabThree';
+export default class TabsExample extends Component {
+  render() {
+    return (
+      <Container style={{backgroundColor: '#2ecc71'}}>
+        <Header
+          style={{backgroundColor: '#2ecc71'}}
+          androidStatusBarColor="#2ecc71">
+          <Left />
+          <Body>
+            <Title>Plug & Plant</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Tabs>
+          <Tab
+            tabStyle={{backgroundColor: '#2ecc71'}}
+            activeTabStyle={{backgroundColor: '#2ecc71'}}
+            activeTextStyle={{color: '#fff'}} textStyle={{color:'white'}}
+            heading="Menu 1">
+            <Tab1 />
+          </Tab>
+          <Tab
+            tabStyle={{backgroundColor: '#2ecc71'}}
+            activeTabStyle={{backgroundColor: '#2ecc71'}}
+            activeTextStyle={{color: '#fff'}} textStyle={{color:'white'}}
+            heading="Menu 2">
+            <Tab2 />
+          </Tab>
+          <Tab
+            tabStyle={{backgroundColor: '#2ecc71'}}
+            activeTabStyle={{backgroundColor: '#2ecc71'}}
+            activeTextStyle={{color: '#fff'}} textStyle={{color:'white'}}
+            heading="Menu 3">
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
---
+++ src/screens/tabs/tabOne.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Button } from 'native-base';
+export default class ListThumbnailExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png' }} />
+              </Left>
+              <Body>
+                <Text>Title</Text>
+                <Text note numberOfLines={2}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png' }} />
+              </Left>
+              <Body>
+                <Text>Title</Text>
+                <Text note numberOfLines={2}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png' }} />
+              </Left>
+              <Body>
+                <Text>Title</Text>
+                <Text note numberOfLines={2}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png' }} />
+              </Left>
+              <Body>
+                <Text>Title</Text>
+                <Text note numberOfLines={2}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail square source={{ uri: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png' }} />
+              </Left>
+              <Body>
+                <Text>Title</Text>
+                <Text note numberOfLines={2}>Its time to build a difference . .</Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
